0a22575178de6f3090aa8571c8f6a987159cd405
infinivis <romain.cerantola@heig-vd.ch>
2015-06-08 11:15:55 +0200

    test TreeCanvas

---
+++ public/js/canvas.js
@@ -0,0 +1,56 @@
+ "use strict";
+ var widthCanevas = 800;
+ var heightCanevas = 600;
+            
+            class Tree {
+                constructor(position){
+                    this.position = position;
+                    
+                    this.size = new Victor(10, 50);
+                }
+                
+            draw(c) {
+               c.fillStyle = "#8b4726";
+               c.fillRect(this.position.x, this.position.y, this.size.x, this.size.y);
+               
+            }
+            }
+            
+            var canvas = document.getElementById('canvas');
+            canvas.width = widthCanevas;//window.innerWidth;
+            canvas.height = heightCanevas; //window.innerHeight;
+            var c = canvas.getContext('2d');
+            c.fillStyle = "#ffffff";
+
+
+            var x = 0;
+            var y = 0;
+            
+            var img = document.getElementById("treeBaby");
+            
+            var tree = new Tree(new Victor(widthCanevas/2, heightCanevas/2));
+////////execution///////////////////////////////////////
+            function draw()
+            {
+                c.clearRect(0, 0, window.innerWidth, window.innerHeight);
+                x += 1;
+                y = y+Math.pow(0.1, x);
+
+                c.beginPath();
+                c.rect(x, y, 20, 20);
+                c.stroke();
+                c.closePath();
+                
+                
+                tree.draw(c);
+                tree.size.addY(new Victor(0, Math.pow(x, 3/4)));
+                c.drawImage(img, widthCanevas/2, heightCanevas/2, 200, 200 );
+                requestAnimationFrame(draw);
+                
+            }
+            
+            draw();
+            
+            
+            
+            
---
+++ public/js/testCanvas.js
@@ -0,0 +1,62 @@
+function point(x,y){
+	this.x=x;
+	this.y=y;
+}
+function CreateCanvas(elementId){
+	var canvas = new Object();
+	canvas.canvas=document.getElementById(elementId);
+	canvas.context=canvas.canvas.getContext('2d');
+	
+	canvas.mouse=new point(0,0);
+	canvas.inside=false;
+	canvas.last_mouse=new point(0,0);
+	canvas.delta=new point(0,0);
+	
+	
+	canvas.canvas.onmousemove = function (event){
+		canvas.last_mouse.x=canvas.mouse.x;
+		canvas.last_mouse.y=canvas.mouse.y;
+		
+		canvas.mouse.x=event.clientX-canvas.canvas.offsetLeft;
+		canvas.mouse.y=event.clientY-canvas.canvas.offsetTop;
+		
+		canvas.delta.x=canvas.mouse.x-canvas.last_mouse.x;
+		canvas.delta.y=canvas.mouse.y-canvas.last_mouse.y;
+	};
+	canvas.canvas.onmouseout = function (event){
+		canvas.inside=false;
+	}
+	canvas.canvas.onmouseover = function (event){
+		canvas.inside=true;
+		
+		canvas.mouse.x=event.clientX-canvas.canvas.offsetLeft;
+		canvas.mouse.y=event.clientY-canvas.canvas.offsetTop;
+		canvas.last_mouse.x=canvas.mouse.x;
+		canvas.last_mouse.y=canvas.mouse.y;
+		canvas.delta.x=0;
+		canvas.delta.y=0;
+	}
+
+	return canvas;
+}
+
+
+
+
+
+//
+//         function draw_linie(){
+//             context.save();
+//             context.beginPath();
+//             context.moveTo(this.x1,this.y1);
+//             context.lineTo(this.x2,this.y2);
+//             context.strokeStyle = 'black';
+//             context.stroke();
+//             context.restore();
+//         }
+//
+//
+//
+//         canvas.onmousedown=start_draw;
+//         canvas.onmouseup=stop_draw;
+//         canvas.onmouseout=stop_draw;
